e14aba650851fc6b6ffa085da4e531eb303e7e62
PriyankaShambhanagoudar <priyassss789@email.com>
2022-01-24 01:20:27 +0530

    Animating Lists

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,5 +1,5 @@
 import React from "react";
-import CSSTransition from "react-transition-group/Transition";
+import CSSTransition from "react-transition-group/CSSTransition";
 
 import "./Modal.css";
 
@@ -15,7 +15,13 @@ const modal = (props) => {
             unmountOnExit
             in={props.show}
             timeout={animatingTiming}
-            classNames="fade-slide" >
+            classNames={{
+                enter: '',
+                enterActive: 'ModalOpen',
+                exit: '',
+                exitActive: 'ModalClosednp',
+
+            }} >
             <div className="Modal">
                 <h1>A Modal</h1>
                 <button className="Button" onClick={props.closed}>
