29a27d37ff57c6fb7ba2461fe9f66b283b63c046
PriyankaShambhanagoudar <priyassss789@email.com>
2022-01-24 00:29:07 +0530

    Wrapping the Transition Component and Animation Timings

--- src/App.js
+++ src/App.js
@@ -33,31 +33,29 @@ class App extends Component {
           Toggle
         </button>
         <br />
-        <Transition in={this.state.showBlock} timeout={1000} mountOnEnter unmountOnExit >
-          {state => (
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+        >
+          {(state) => (
             <div
               style={{
                 backgroundColor: "red",
                 width: 100,
                 height: 100,
                 margin: "auto",
-                transition: 'opacity 1s ease-out',
-                opacity: state === 'exiting' ? 0 : 1
+                transition: "opacity 1s ease-out",
+                opacity: state === "exiting" ? 0 : 1,
               }}
             ></div>
           )}
-
         </Transition>
 
-        <Transition in={this.state.modalIsOpen} timeout={300} mountOnEnter unmountOnExit>
-          {state => (
-            <Modal show={state} closed={this.closeModal} />
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
 
-          )}
-        </Transition>
-        {this.state.modalIsOpen ? (
-          <Backdrop show />
-        ) : null}
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>
           Open Modal
         </button>
